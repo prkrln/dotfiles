ca439f51416b8bf06a68c9fbc2ad2908c7366528
Hippo5412 <parkerlane003@gmail.com>
2025-11-20 12:44:07 -0600

    added copilot :(

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,6 +17,7 @@ vim.pack.add({
 	{src = "https://github.com/mason-org/mason.nvim"},
 	{src = "https://github.com/mason-org/mason-lspconfig.nvim"},
 	{src = "https://github.com/echasnovski/mini.nvim"},
+	{src = "https://github.com/github/copilot.vim"},
 })
 
 vim.cmd("colorscheme gruvbox")
